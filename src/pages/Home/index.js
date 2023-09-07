cc43ed5dea111b77b343512accc03819257e8573
stvnus <stvnus98@gmail.com>
2023-09-07 23:02:31 +0700

    feat : add modal image dan components Images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react';
 import Navbar from '../../components/organism/Navbar';
 import Footer from '../../components/organism/Footer';
 import useAxios from '../../hooks/axios';
+import Image from '../../components/molecules/Images'; // Mengimpor komponen Image
 
-function Index() {
+function Home() {
   const { response, isLoading, error, fetchData } = useAxios('/photos/random?count=12');
 
   useEffect(() => {
@@ -22,9 +23,11 @@ function Index() {
           <h1 className="text-2xl font-bold mt-20 mb-2 text-center">Random Unsplash Images</h1>
           <div className="flex flex-wrap justify-center">
             {response.map((image) => (
-              <div key={image.id} className="w-1/4 p-2">
-                <img src={image.urls.small} alt={image.description} className="h-72 w-full object-cover rounded-lg shadow-md" />
-              </div>
+              <Image
+                key={image.id}
+                src={image.urls.small}
+                alt={image.description}
+              />
             ))}
           </div>
         </div>
@@ -35,4 +38,4 @@ function Index() {
   );
 }
 
-export default Index;
+export default Home;
