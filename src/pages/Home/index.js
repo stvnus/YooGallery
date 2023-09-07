e2dc3e176d28939f59febf9083d84103adfd4e7c
stvnus <stvnus98@gmail.com>
2023-09-07 22:37:26 +0700

    style : adjust image size for Unsplash API image

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,10 +4,10 @@ import Footer from '../../components/organism/Footer';
 import useAxios from '../../hooks/axios';
 
 function Index() {
-  const { response, isLoading, error, fetchData } = useAxios('/photos/random?count=10');
+  const { response, isLoading, error, fetchData } = useAxios('/photos/random?count=12');
 
   useEffect(() => {
-    fetchData('/photos/random?count=10');
+    fetchData('/photos/random?count=12');
   }, []); 
 
   return (
@@ -19,15 +19,18 @@ function Index() {
         <p>Error: {error.message}</p>
       ) : (
         <div>
-          <h1>Random Unsplash Images</h1>
-          <div className="image-list">
+          <h1 className="text-2xl font-bold mt-20 mb-2 text-center">Random Unsplash Images</h1>
+          <div className="flex flex-wrap justify-center">
             {response.map((image) => (
-              <img key={image.id} src={image.urls.regular} alt={image.description} />
+              <div key={image.id} className="w-1/4 p-2">
+                <img src={image.urls.small} alt={image.description} className="h-72 w-full object-cover rounded-lg shadow-md" />
+              </div>
             ))}
           </div>
         </div>
       )}
 
+      <Footer />
     </div>
   );
 }
