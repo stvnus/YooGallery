b5b27aa917d5fa2e7764feb6a94bf377ddcb9fd3
stvnus <stvnus98@gmail.com>
2023-09-07 09:28:52 +0700

    feat : add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
+import Router from "./router";
 import './index.css';
 
 function App() {
-  return (
-    <div className="bg-blue-500 text-white p-4 text-white">
-      Ini adalah komponen dengan Tailwind CSS
-    </div>
-  );
+	return (
+		<>
+			<Router />
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline text-center">
+         Hello world!
+         </h1>
+    </div>
+  )
+}
+
+export default index
