4e84b9b45c5ac9d52f7c24cd39318b373a9a85b9
stvnus <stvnus98@gmail.com>
2023-09-07 21:26:20 +0700

    feat : using axios to consume unsplash API on homepage

--- src/hooks/axios/index.js
+++ src/hooks/axios/index.js
@@ -1,35 +1,38 @@
-import axios from "axios";
-import { useEffect, useState } from "react"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const useAxios = (param) => {
   const [response, setResponse] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
-  
-  axios.defaults.baseURL = 'https://api.unsplash.com';
+
+  const unsplashAccessKey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY; // Ambil token API Unsplash dari variabel lingkungan Anda
+  const baseURL = 'https://api.unsplash.com';
+  axios.defaults.baseURL = baseURL;
+  axios.defaults.headers.common['Authorization'] = `Client-ID ${unsplashAccessKey}`;
 
   const fetchData = async (url) => {
     try {
       setIsLoading(true);
-      const res = await axios(url);
-      setResponse(res.data.results);
-    } catch(err) {
-      setError(err)
+      const res = await axios.get(url);
+      setResponse(res.data);
+    } catch (err) {
+      setError(err);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData(param);
-  }, [param])
+  }, [param]);
 
   return {
     response,
     isLoading,
     error,
-    fetchData: url => fetchData(url)
-  }
-}
+    fetchData: (url) => fetchData(url),
+  };
+};
 
-export default useAxios
+export default useAxios;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import Navbar from '../../components/organism/Navbar';
 import Footer from '../../components/organism/Footer';
-function index() {
+import useAxios from '../../hooks/axios';
+
+function Index() {
+  const { response, isLoading, error, fetchData } = useAxios('/photos/random?count=10');
+
+  useEffect(() => {
+    fetchData('/photos/random?count=10');
+  }, []); 
+
   return (
     <div>
-        <Navbar />
-   
-        <Footer />
+      <Navbar />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <div>
+          <h1>Random Unsplash Images</h1>
+          <div className="image-list">
+            {response.map((image) => (
+              <img key={image.id} src={image.urls.regular} alt={image.description} />
+            ))}
+          </div>
+        </div>
+      )}
+
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
